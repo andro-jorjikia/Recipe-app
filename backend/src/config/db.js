7e00bb13bd9c29ad974193b5a8402f0b8c7fdeda
andro jorjikia <129059061+andro-jorjikia@users.noreply.github.com>
2025-06-27 13:48:46 +0400

    working on api deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless"; 
 import { drizzle } from "drizzle-orm/neon-http"; 
-import { ENV } from "../config/env.js"; 
-import * as schema from "./schema.js"; 
+import { ENV } from "./env.js"; 
+import * as schema from "../db/schema.js"; 
 
 const sql = neon(ENV.DATABASE_URL); 
 export const db = drizzle(sql, { schema }); 
